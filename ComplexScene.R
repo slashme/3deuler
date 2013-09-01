c5b659fd08d8f884616794d1799ac02e8177f3a7
David Richfield <davidrichfield@gmail.com>
2013-09-01 23:26:10 +0200

    load rgl library; add comments

--- ComplexScene.R
+++ ComplexScene.R
@@ -1,7 +1,17 @@
+# Load rgl library
+require(rgl)
+# Create a vector from -2π to 2π
 re = (-10:10)*pi/5
+# Create a vector from -2πi to 2πi
 im = re*1i
+# Define the exponential function on the sum of the two vectors
 f <- function(re,im) exp(re+im)
+# Create a square matrix with the values of the function over the domain of the two vectors
 complexsurf=outer(re,im,f)
+# Create a square matrix containing the real value of the complex surface
 realsurf=Re(complexsurf)
+# Open a 3D canvas
 open3d()
+# Plot a surface using the real-valued surface calculated above, coloured red, 50% transparent.  
+# TODO: figure out how to set the scene so that you don't have to zoom in after this step.
 surface3d(re,re,realsurf,color=c("red"),alpha=0.5)
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 3deuler
 =======
 
-Create an animation of Euler's equation in R using the rgl library
+Project aim:
+Create an animation of Euler's equation in R using the rgl library, following the discussion at https://en.wikipedia.org/wiki/Wikipedia:Graphics_Lab/Illustration_workshop#RfC_-_Visualization_of_exponential_function
+
+Progress so far: Installed R and rgl on Ubuntu; created a 3D surface of the exponentiation of complex numbers.
+
+Installation instructions on Ubuntu:
+sudo apt-get install r-base
+sudo apt-get install r-cran-rgl
+
