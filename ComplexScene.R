9bf46ca52474a8f071816833f62aef767f75a320
David Richfield <davidrichfield@gmail.com>
2013-09-02 18:47:24 +0200

    Truncate graph, only add axes in center.

--- ComplexScene.R
+++ ComplexScene.R
@@ -10,14 +10,21 @@ f <- function(re,im) exp(re+im)
 complexsurf=outer(re,im,f)
 # Create a square matrix containing the real value of the complex surface
 realsurf=Re(complexsurf)
+# Truncate the matrix
+realsurf[(realsurf < -10)] = NA
+realsurf[(realsurf >  10)] = NA
 # Open a 3D canvas
 open3d()
 # Plot a surface using the real-valued surface calculated above, coloured red, 50% transparent.  
 # TODO: figure out how to set the scene so that you don't have to zoom in after this step.
 surface3d(re,re,realsurf,color=c("red"),alpha=0.5)
-# Add axes - note: tick length depends on the bounding box size, so at the moment, these axes look really bad.  Options: truncate the data, or somehow resize the bounding box.
-axes3d()
+# Add axes
+# axes3d()
 axis3d("x",pos=c(0,0,0))
 axis3d("y",pos=c(0,0,0))
 labels=T
 # Next step: use the technique alluded to in http://stackoverflow.com/questions/15717267/how-to-draw-parametric-3d-curve-with-smoothing-in-r to generate a parametric curve.
+# segments3d(c(re,sinfunction(re),re*0.0)) doesn't work: need to generate an array
+sinfunction = function(x) sin(x)
+sincurve = cbind(re, sinfunction(re), re*0)
+
